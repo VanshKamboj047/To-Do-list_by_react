f10213fdfb1b68c5ebc97e9fb5548d1d9c59c5e6
vansh <vanshkamboj@gmail.com>
2025-07-13 17:38:17 +0530

    final version of To-Do List

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/1-todo-app-version-1/' // 👈 Use your GitHub repo name here
+  base: '/To-Do-list_by_react/' // 
 })
